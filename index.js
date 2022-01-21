82b63468806ace1a45439133c0bec0d437d51e08
Abhishek gupta <79398278+abhishek6377@users.noreply.github.com>
2022-01-21 17:18:45 +0530

    this is portfolio

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("Hello world");
+// document.querySelector('.cross').style.display='none';
+document.querySelector('.hamburger').addEventListener("click",()=>{
+    document.querySelector('slidebar').classList.toggle('slidebargo')
+});
